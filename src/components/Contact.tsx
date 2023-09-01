32ebdf94d634beae94455ad48c464338fa7cd8ae
rintaro1029 <tomitaka1029@gmail.com>
2023-09-01 15:04:44 +0900

    Contactコンポーネント作成

---
+++ src/components/Contact.tsx
@@ -0,0 +1,48 @@
+import Image from "next/image";
+import React from "react";
+
+const Contact = () => {
+  return (
+    <>
+      <div className="  container mx-auto mt-48 flex  w-full flex-col items-center justify-between px-8 md:px-14 lg:px-24">
+        <section className="mb-32 w-full  ">
+          <div className="">
+            <div className="flex">
+              <div className=" mr-12 text-6xl font-bold">Contact Me</div>
+
+              <Image src="/SNS/twitter.svg" alt="image" width={40} height={40} className="mx-2  rounded-full" />
+              <Image src="/SNS/instagram.png" alt="image" width={55} height={55} className="z-10  mx-2 rounded-full " />
+              <Image src="/SNS/facebook.svg" alt="image" width={40} height={40} className="mx-2  rounded-full " />
+            </div>
+
+            <div className="lg: mt-16  gap-8  space-y-10 lg:gap-32">
+              <div>
+                <label htmlFor="" className="mb-2 block text-xl font-bold text-white ">
+                  お名前
+                </label>
+                <input type="text" className="w-1/2 border-inputBorder bg-input p-4" />
+              </div>
+              <div>
+                <label htmlFor="" className="mb-2 block text-xl font-bold text-white ">
+                  メールアドレス
+                </label>
+                <input type="text" className=" w-1/2 border-inputBorder bg-input p-4" />
+              </div>
+              <div>
+                <label htmlFor="" className="mb-2 block text-xl font-bold text-white">
+                  メッセージ
+                </label>
+                <textarea className="h-32   w-2/3 resize-none overflow-hidden border-inputBorder bg-input  text-xl " />
+              </div>
+              <button className="  mt-10  w-32 rounded bg-theme p-2 font-bold transition-all duration-300 hover:bg-pink-300">
+                <p> Submit</p>
+              </button>
+            </div>
+          </div>
+        </section>
+      </div>
+    </>
+  );
+};
+
+export default Contact;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,25 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        sans: ["Poppins", "sans-serif"],
+      },
+      colors: {
+        main: "#050505",
+        "selected-text": "#304ADE",
+        theme: "#E73BE2",
+        input: "#2A2A33",
+        inputBorder: "#565666",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
