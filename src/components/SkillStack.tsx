7d2adc44e5ae1d7c00e4e64b051b06d2c61405cf
rintaro1029 <tomitaka1029@gmail.com>
2023-09-01 10:55:45 +0900

    スキルスタックコンポーネントの追加

---
+++ src/components/SkillStack.tsx
@@ -0,0 +1,97 @@
+import Image from "next/image";
+import React from "react";
+
+const SkillStack = () => {
+  return (
+    <>
+      <div className="  mt-50 container mx-auto flex  w-full flex-col items-center justify-between px-8 md:px-14 lg:px-24">
+        <section className="w-full">
+          <div className="mb-10 text-4xl font-bold">Skill Stack</div>
+        </section>
+        <div className=" w-full ">
+          <div className="grid grid-cols-2  justify-items-center gap-16 md:grid-cols-4">
+            <Image
+              src="/SkillStack/javascript.svg"
+              alt="image"
+              width={50}
+              height={50}
+              className="  rounded-md object-cover"
+            />
+            <Image
+              src="/SkillStack/react.svg"
+              alt="image"
+              width={50}
+              height={50}
+              className="  rounded-md object-cover"
+            />
+            <Image
+              src="/SkillStack/typescript-icon.svg"
+              alt="image"
+              width={50}
+              height={50}
+              className="  rounded-md object-cover"
+            />
+            <Image
+              src="/SkillStack/nextjs-icon.svg"
+              alt="image"
+              width={50}
+              height={50}
+              className="  rounded-md object-cover"
+            />
+            <Image
+              src="/SkillStack/tailwindcss-icon.svg"
+              alt="image"
+              width={50}
+              height={50}
+              className="  rounded-md object-cover"
+            />
+            <Image
+              src="/SkillStack/flutter.svg"
+              alt="image"
+              width={50}
+              height={50}
+              className="  rounded-md object-cover"
+            />{" "}
+            <Image
+              src="/SkillStack/figma.svg"
+              alt="image"
+              width={50}
+              height={50}
+              className="  rounded-md object-cover"
+            />
+            <Image
+              src="/SkillStack/threejs.jpg"
+              alt="image"
+              width={50}
+              height={50}
+              className="  rounded-md object-cover"
+            />
+            <Image
+              src="/SkillStack/nodejs-icon-alt.svg"
+              alt="image"
+              width={50}
+              height={50}
+              className="  rounded-md object-cover"
+            />
+            <Image
+              src="/SkillStack/python.svg"
+              alt="image"
+              width={50}
+              height={50}
+              className="  rounded-md object-cover"
+            />
+            <Image
+              src="/SkillStack/supabase-icon.svg"
+              alt="image"
+              width={50}
+              height={50}
+              className="  rounded-md object-cover"
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SkillStack;
