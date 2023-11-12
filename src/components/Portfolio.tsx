8b2ce14a6379048aa89a49571afb2f36875db629
rintaro1029 <tomitaka1029@gmail.com>
2023-11-12 21:54:51 +0900

    ✨ アニメーションの導入

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -1,20 +1,55 @@
 import Image from "next/image";
 import React from "react";
 import PortfolioItems from "./PortfolioItems";
+import { motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 
 const Portfolio = () => {
+  const { ref, inView } = useInView({
+    threshold: 0.3, // 40%が見えた時点でトリガー
+    triggerOnce: true, // 一度だけトリガー
+  });
+
+  const portfolioData = [
+    { title: "3D-Portfolio", imageUrl: "/images/universe.jpg", link: "" },
+    { title: "Accounting System", imageUrl: "/images/accounting-system.png", link: "" },
+    { title: "Chat App", imageUrl: "/images/chat-app.jpeg", link: "" },
+    { title: "Cafeteria App", imageUrl: "/images/cafeteria.jpg", link: "" },
+    // 他のPortfolioItemsデータを追加
+  ];
+
+  const itemVariants = {
+    hidden: { opacity: 0, x: 0, y: 30 },
+    visible: { opacity: 1, x: 0, y: 0 },
+  };
+
   return (
     <>
-      <div className=" container mx-auto mb-14 mt-80 flex   w-full flex-col items-center justify-between px-8 md:px-14 lg:px-24">
-        <section className="w-full">
+      <div
+        ref={ref}
+        className="container mx-auto mb-14 mt-80 flex w-full flex-col items-center justify-between px-8 md:px-14 lg:px-24"
+      >
+        <motion.section
+          className="w-full"
+          variants={itemVariants}
+          initial="hidden"
+          animate={inView ? "visible" : "hidden"}
+        >
           <div className="mb-32 text-6xl font-bold">My Portfolio</div>
-        </section>
-
-        <div className="grid gap-6  md:grid-cols-2">
-          <PortfolioItems title="3D-Portfolio" imageUrl="/images/universe.jpg" link="" />
-          <PortfolioItems title="Accounting System" imageUrl="/images/accounting-system.png" link="" />
-          <PortfolioItems title="Chat App" imageUrl="/images/chat-app.jpeg" link="" />
-          <PortfolioItems title="Cafeteria App" imageUrl="/images/cafeteria.jpg" link="" />
+        </motion.section>
+        <div className="w-full">
+          <motion.div
+            className="grid gap-6 md:grid-cols-2"
+            variants={{ visible: { transition: { staggerChildren: 0.4 } } }}
+            initial="hidden"
+            animate={inView ? "visible" : "hidden"}
+          >
+            {portfolioData.map((item, index) => (
+              <motion.div key={index} variants={itemVariants}>
+                <PortfolioItems title={item.title} imageUrl={item.imageUrl} link={item.link} />
+              </motion.div>
+            ))}
+          </motion.div>
         </div>
       </div>
     </>
--- src/components/SkillStack.tsx
+++ src/components/SkillStack.tsx
@@ -1,33 +1,63 @@
 import Image from "next/image";
+import { motion } from "framer-motion";
 import React from "react";
+import { useInView } from "react-intersection-observer";
 
 const SkillStack = () => {
+  const { ref, inView } = useInView({
+    threshold: 0.5, // 50%が見えた時点でトリガー
+    triggerOnce: true, // 一度だけトリガー
+  });
+
+  // 画像のパスを配列で定義
+  const images = [
+    "/skillStack/javascript.svg",
+    "/skillStack/react.svg",
+    "/skillStack/typescript-icon.svg",
+    "/skillStack/nextjs-icon.svg",
+    "/skillStack/tailwindcss-icon.svg",
+    "/skillStack/flutter.svg",
+    "/skillStack/figma.svg",
+    "/skillStack/threejs.jpg",
+    "/skillStack/nodejs-icon-alt.svg",
+    "/skillStack/python.svg",
+    "/skillStack/supabase-icon.svg",
+  ];
+
+  // 各アイテムのアニメーション定義
+  const itemVariants = {
+    hidden: { opacity: 0, x: 0, y: 30 },
+    visible: { opacity: 1, x: 0, y: 0 },
+  };
+
   return (
     <>
-      <div className="  container mx-auto  mt-80 flex  w-full flex-col items-center justify-between px-8 md:px-14 lg:px-24">
-        <section className="w-full">
+      <div
+        ref={ref}
+        className="container mx-auto mt-80 flex w-full flex-col items-center justify-between px-8 md:px-14 lg:px-24"
+      >
+        <motion.section
+          className="w-full"
+          variants={itemVariants}
+          initial="hidden"
+          animate={inView ? "visible" : "hidden"}
+        >
           <div className="mb-32 text-6xl font-bold">Skill Stack</div>
-        </section>
-        <div className=" w-full ">
-          <div className="grid grid-cols-2  justify-items-center gap-16 md:grid-cols-4">
-            <Image src="/skillStack/javascript.svg" alt="image" width={50} height={50} className="  rounded-md " />
-            <Image src="/skillStack/react.svg" alt="image" width={50} height={50} className="  rounded-md " />
-            <Image src="/skillStack/typescript-icon.svg" alt="image" width={50} height={50} className="  rounded-md " />
-            <Image src="/skillStack/nextjs-icon.svg" alt="image" width={50} height={50} className="  rounded-md " />
-            <Image
-              src="/skillStack/tailwindcss-icon.svg"
-              alt="image"
-              width={50}
-              height={50}
-              className="  rounded-md "
-            />
-            <Image src="/skillStack/flutter.svg" alt="image" width={50} height={50} className="  rounded-md " />
-            <Image src="/skillStack/figma.svg" alt="image" width={50} height={50} className="  rounded-md " />
-            <Image src="/skillStack/threejs.jpg" alt="image" width={50} height={50} className="  rounded-md " />
-            <Image src="/skillStack/nodejs-icon-alt.svg" alt="image" width={50} height={50} className="  rounded-md " />
-            <Image src="/skillStack/python.svg" alt="image" width={50} height={50} className="  rounded-md " />
-            <Image src="/skillStack/supabase-icon.svg" alt="image" width={50} height={50} className="  rounded-md " />
-          </div>
+        </motion.section>
+        <div className="w-full">
+          <motion.div
+            className="grid grid-cols-2 justify-items-center gap-16 md:grid-cols-4"
+            variants={{ visible: { transition: { staggerChildren: 0.2 } } }}
+            initial="hidden"
+            animate={inView ? "visible" : "hidden"}
+          >
+            {/* 画像をマッピングして表示 */}
+            {images.map((src, index) => (
+              <motion.div key={index} variants={itemVariants}>
+                <Image src={src} alt="image" width={50} height={50} className="rounded-md" />
+              </motion.div>
+            ))}
+          </motion.div>
         </div>
       </div>
     </>
